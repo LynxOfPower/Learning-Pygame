339ccc2de26485dcdbbf3543b6e4046bab5a900c
LynxOfPower <isaiah.durey@gmail.com>
2021-09-03 23:20:57 -0400

    first commit

---
+++ LearningPygame.py
@@ -0,0 +1,62 @@
+#I guess we're learning pygame. let's see how this goes!
+
+
+# REMINDERS FOR NEXT STREAM:
+# - Get sprites for grid [X] (This is pretty much started, the question is if I'm doing it right)
+# - Get a hover sprite up for grid sprites. (Possibly done with a def hover() in an external file?) []
+# - work on UI on the right hand of the screen []
+
+
+import pygame
+print(pygame.ver)
+
+pygame.init()
+
+screen_width = 800
+screen_height = 800
+
+screen = pygame.display.set_mode([screen_width, screen_height]) # Drawing Window
+
+
+#So I got the sprite working, now how do I make it do stuff?
+# Sprites for the soil grid
+class GridTiles(pygame.sprite.Sprite):
+    def __init__(self):
+        super(GridTiles, self).__init__()
+        self.surf = pygame.Surface((32, 32))
+        self.image = pygame.image.load("images/soilX5.png")
+
+tile = GridTiles() # initialize the GridTiles sprite in this variable.
+
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((37, 36, 37))
+
+    # #TOP ROW
+    screen.blit(tile.image, (100, 200))
+    screen.blit(tile.image, (275, 200))
+    screen.blit(tile.image, (450, 200))
+
+    # #MIDDLE ROW
+    screen.blit(tile.image, (100, 375))
+    screen.blit(tile.image, (275, 375))
+    screen.blit(tile.image, (450, 375))
+
+    # #BOTTOM ROW
+    screen.blit(tile.image, (100, 550))
+    screen.blit(tile.image, (275, 550))
+    screen.blit(tile.image, (450, 550))
+
+    pygame.display.flip()
+
+pygame.quit()
+
+# This has been coded by Isaiah Durey
+# If it's coded badly, to bad, get rekt nerd.
+# Just kidding, I'm sorry D:
---
+++ README.txt
@@ -0,0 +1,6 @@
+This game was made as a small project for me. I don't know if I'm doing things with best practice but I'm trying my best.
+
+
+Notes:
+- The image naming convention is Image-NameXZoom. This is to denote what it is and how big it will be in game. The reason I keep the original sizing is if I need to re-import it
+into piskelapp.com
